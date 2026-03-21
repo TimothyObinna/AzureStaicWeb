2c9a0d04642b299066824d08363d1fe4068a4399
Timothy <ewatimothyobinna@gmail.com>
2026-03-21 09:40:52 +0100

    🚀 Latest commit - Static Website Deployment

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-<<<<<<< HEAD
 # Exit immediately if a command fails
 set -e
 
@@ -102,32 +101,3 @@ echo "📦 Storage Account: $STORAGE_ACCOUNT"
 echo "=====================================\n"
 
 
-=======
-RESOURCE_GROUP=cloudproject1
-STORAGE_ACCOUNT=obistaticweb
-LOCATION=uksouth
-
-# This part of the script create the resource group
-az group create --name $RESOURCE_GROUP --location $LOCATION
-
-# This part of the script creat azure storage account
-az storage account create \
-  --name $STORAGE_ACCOUNT \
-  --resource-group $RESOURCE_GROUP \
-  --location $LOCATION \
-  --sku Standard_LRS
-
-
-# This part of the script update the storage account and indicate the index page and the error page
-az storage blob service-properties update \
-  --account-name $STORAGE_ACCOUNT \
-  --static-website \
-  --index-document index.html \
-  --404-document 404.html
-
-# This part of the sript upload the files to the storage account
-az storage blob upload-batch \
-  --account-name $STORAGE_ACCOUNT \
-  --source . \
-  --destination '$web'
->>>>>>> 2ac624b8adc8cd199146d561ccb3c5ae5acc8f40
